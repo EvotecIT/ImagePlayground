d620409c27966b9a4e139d5c77a8418ca9280589
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-12-05 18:51:10 +0100

    Updates

--- Public/New-QRCode.ps1
+++ Public/New-QRCode.ps1
@@ -5,32 +5,34 @@
         [Parameter(Mandatory)][string] $FilePath,
         [ValidateSet('png', 'webp')][string] $FileFormat = 'png',
         [int] $Width = 256,
-        [int] $Height = 256
+        [int] $Height = 256,
+        [switch] $Transparent
     )
-    try {
-        $FileStream = [System.IO.FileStream]::new($FilePath, [System.IO.FileMode]::OpenOrCreate)
-    } catch {
-        if ($PSBoundParameters.ErrorAction -eq 'Stop') {
-            Write-Error -ErrorRecord $_
-            return
-        } else {
-            Write-Warning -Message "New-QRCode - Couldn't prepare filestream. Error: $($_.Exception.Message)"
-        }
-    }
-    $Vector = [SkiaSharp.QrCode.Image.Vector2Slim]::new($Width, $Height)
-    $Encoding = [SkiaSharp.SKEncodedImageFormat]::$FileFormat
 
-    try {
-        $QrCode = [SkiaSharp.QrCode.Image.QrCode]::new($Content, $Vector, $Encoding)
-        $QrCode.GenerateImage($FileStream)
-    } catch {
-        if ($PSBoundParameters.ErrorAction -eq 'Stop') {
-            Write-Error -ErrorRecord $_
-            return
-        } else {
-            Write-Warning -Message "New-QRCode - Couldn't generate QR code. Error: $($_.Exception.Message)"
-        }
-    } finally {
-        $FileStream.Close()
+    # https://github.com/codebude/QRCoder/wiki/Advanced-usage---Payload-generators#320-whatsappmessage
+
+
+    $QRCodeGenerator = [QRCoder.QRCodeGenerator]::new()
+    $QRCodeData = $QRCodeGenerator.CreateQrCode($Content, [QRCoder.QRCodeGenerator+ECCLevel]::Q)
+    $QRCode = [QRCoder.QRCode]::new($QRCodeData)
+    $QrCodeImage = $QRCode.GetGraphic(20)
+    if ($Transparent) {
+        $QrCodeImage.MakeTransparent()
     }
+    $QrCodeImage.Save($FilePath)
+
+    <#
+
+    System.Drawing.Bitmap GetGraphic(int pixelsPerModule)
+    System.Drawing.Bitmap GetGraphic(int pixelsPerModule, string darkColorHtmlHex, string lightColorHtmlHex, bool drawQuietZones)
+    System.Drawing.Bitmap GetGraphic(int pixelsPerModule, System.Drawing.Color darkColor, System.Drawing.Color lightColor, bool drawQuietZones)
+    System.Drawing.Bitmap GetGraphic(int pixelsPerModule, System.Drawing.Color darkColor, System.Drawing.Color lightColor, System.Drawing.Bitmap icon, int iconSizePercent, int iconBorderWidth, bool drawQuietZones, System.Nullable[System.Drawing.Color] iconBackgroundC
+    olor)
+
+    QRCoder.QRCodeData CreateQrCode(QRCoder.PayloadGenerator+Payload payload)
+    QRCoder.QRCodeData CreateQrCode(QRCoder.PayloadGenerator+Payload payload, QRCoder.QRCodeGenerator+ECCLevel eccLevel)
+    QRCoder.QRCodeData CreateQrCode(string plainText, QRCoder.QRCodeGenerator+ECCLevel eccLevel, bool forceUtf8, bool utf8BOM, QRCoder.QRCodeGenerator+EciMode eciMode, int requestedVersion)
+    QRCoder.QRCodeData CreateQrCode(byte[] binaryData, QRCoder.QRCodeGenerator+ECCLevel eccLevel)
+    #>
+
 }
