d651d43606f896dbbdb208d9f2249ad705a983f3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-12-19 10:01:55 +0100

    QR code ascsii

---
+++ Public/New-ASCIIQRCode.ps1
@@ -0,0 +1,30 @@
+﻿function New-ASCIIQRCode {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER Content
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    https://github.com/codebude/QRCoder/wiki
+    #>
+    [cmdletBinding()]
+    param(
+        [string] $Content
+    )
+    $QRCodeGenerator = [QRCoder.QRCodeGenerator]::new()
+    $QRCodeData = $QRCodeGenerator.CreateQrCode($Content, [QRCoder.QRCodeGenerator+ECCLevel]::Q)
+    $QRCode = [QRCoder.AsciiQRCode]::new($QRCodeData)
+    $QrCodeImage = $QRCode.GetGraphic(1)
+    if ($Transparent) {
+        $QrCodeImage.MakeTransparent()
+    }
+    $QrCodeImage
+}
