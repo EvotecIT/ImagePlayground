a9dccd6c7464b17833bc8c435cf1b2fad234c474
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-06-17 22:01:57 +0200

    rebuilding functions

--- Public/New-ImageQRCode.ps1
+++ Public/New-ImageQRCode.ps1
@@ -1,38 +1,9 @@
-﻿function New-ImageQRCode {
+﻿function New-QRCode {
+    [Alias('New-ImageQRCode')]
     [cmdletBinding()]
     param(
-        [Parameter(Mandatory)][string] $Content,
-        [Parameter(Mandatory)][string] $FilePath,
-        [ValidateSet('png', 'webp')][string] $FileFormat = 'png',
-        [int] $Width = 256,
-        [int] $Height = 256,
-        [switch] $Transparent
+
     )
 
-    # https://github.com/codebude/QRCoder/wiki/Advanced-usage---Payload-generators#320-whatsappmessage
-
-
-    $QRCodeGenerator = [QRCoder.QRCodeGenerator]::new()
-    $QRCodeData = $QRCodeGenerator.CreateQrCode($Content, [QRCoder.QRCodeGenerator+ECCLevel]::Q)
-    $QRCode = [QRCoder.QRCode]::new($QRCodeData)
-    $QrCodeImage = $QRCode.GetGraphic(20)
-    if ($Transparent) {
-        $QrCodeImage.MakeTransparent()
-    }
-    $QrCodeImage.Save($FilePath)
-
-    <#
-
-    System.Drawing.Bitmap GetGraphic(int pixelsPerModule)
-    System.Drawing.Bitmap GetGraphic(int pixelsPerModule, string darkColorHtmlHex, string lightColorHtmlHex, bool drawQuietZones)
-    System.Drawing.Bitmap GetGraphic(int pixelsPerModule, System.Drawing.Color darkColor, System.Drawing.Color lightColor, bool drawQuietZones)
-    System.Drawing.Bitmap GetGraphic(int pixelsPerModule, System.Drawing.Color darkColor, System.Drawing.Color lightColor, System.Drawing.Bitmap icon, int iconSizePercent, int iconBorderWidth, bool drawQuietZones, System.Nullable[System.Drawing.Color] iconBackgroundC
-    olor)
-
-    QRCoder.QRCodeData CreateQrCode(QRCoder.PayloadGenerator+Payload payload)
-    QRCoder.QRCodeData CreateQrCode(QRCoder.PayloadGenerator+Payload payload, QRCoder.QRCodeGenerator+ECCLevel eccLevel)
-    QRCoder.QRCodeData CreateQrCode(string plainText, QRCoder.QRCodeGenerator+ECCLevel eccLevel, bool forceUtf8, bool utf8BOM, QRCoder.QRCodeGenerator+EciMode eciMode, int requestedVersion)
-    QRCoder.QRCodeData CreateQrCode(byte[] binaryData, QRCoder.QRCodeGenerator+ECCLevel eccLevel)
-    #>
-
+    [ImagePlayground.QrCode]::Generate($Content, $FilePath, $ImageFormat)
 }
---
+++ Public/New-ImageQRCodeWiFi.ps1
@@ -0,0 +1,14 @@
+﻿function New-ImageQRCodeWiFi {
+    [alias('New-QRCodeWiFi')]
+    [cmdletBinding()]
+    param(
+        [string] $SSID,
+        [string] $Password,
+        [string] $FilePath,
+        [System.Drawing.Imaging.ImageFormat] $ImageFormat,
+        [switch] $Transparent
+    )
+
+    [ImagePlayground.QrCode]::GenerateWiFi($ssid, $password, $FilePath, $ImageFormat, $Transparent.IsPresent)
+
+}
