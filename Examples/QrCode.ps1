ef3298e42c90263dd4b3ef3b46197454fb1e569e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-06-17 22:14:48 +0200

    Working examples

---
+++ Examples/QrCode.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
+
+New-ImageQRCode -Content 'https://evotec.xyz' -FilePath "$PSScriptRoot\Samples\QRCode.png"
+New-ImageQRCode -Content 'https://evotec.xyz' -FilePath "$PSScriptRoot\Samples\QRCodeTransparent.png" -Transparent
--- Examples/QrCodeWiFi.ps1
+++ Examples/QrCodeWiFi.ps1
@@ -1,7 +1,3 @@
 ﻿Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
 
-#New-ImageQRCode -Content 'https://evotec.xyz' -FilePath "$PSScriptRoot\Samples\QRCode1.png"
-#New-ImageQRCode -Content 'https://evotec.xyz' -FilePath "$PSScriptRoot\Samples\QRCode2.png" -Transparent
-
-
 New-ImageQRCodeWiFi -SSID 'Evotec' -Password 'Evotec' -FilePath "$PSScriptRoot\Samples\QRCodeWiFi.png"
