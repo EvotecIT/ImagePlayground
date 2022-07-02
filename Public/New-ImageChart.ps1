a46f5ec8a4cc1c15b78aaa6cff53433cb7bd519d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-02 08:23:46 +0200

    Basic Bar support

--- Public/New-ImageChart.ps1
+++ Public/New-ImageChart.ps1
@@ -1,8 +1,43 @@
 ﻿function New-ImageChart {
     [cmdletBinding()]
     param(
-
+        [scriptblock] $ChartsDefinition,
+        [int] $Width = 600,
+        [int] $Height = 400,
+        [string] $FilePath,
+        [switch] $Show
     )
 
-    [ScottPlot.Plot]
+    $Values = [System.Collections.Generic.List[double]]::new()
+    $Labels = [System.Collections.Generic.List[string]]::new()
+    $Positions = [System.Collections.Generic.List[int]]::new()
+    $Plot = [ScottPlot.Plot]::new($Width, $Height)
+    $Position = 0
+
+    if ($ChartsDefinition) {
+        $OutputDefintion = & $ChartsDefinition
+        foreach ($Definition in $OutputDefintion) {
+            if ($Definition.ObjectType -eq 'Bar') {
+                $Values.Add($Definition.Value)
+                $Labels.Add($Definition.Name)
+                $Positions.Add($Position)
+                $Position++
+            }
+        }
+    }
+    if ($Values) {
+        $null = $Plot.AddBar($Values)
+    }
+    if ($Labels) {
+        $null = $Plot.XTicks($Positions, $Labels)
+    }
+
+    if (-not $FilePath) {
+        $FilePath = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "$($([System.IO.Path]::GetRandomFileName()).Split('.')[0]).png")
+    }
+    $null = $Plot.SaveFig($FilePath)
+
+    if ($Show) {
+        Invoke-Item -LiteralPath $FilePath
+    }
 }
--- Public/New-ImageChartBar.ps1
+++ Public/New-ImageChartBar.ps1
@@ -1,6 +1,12 @@
 ﻿function New-ImageChartBar {
     [cmdletBinding()]
     param(
-
+        [alias('Label')][string] $Name,
+        [object] $Value
     )
+    [PSCustomObject] @{
+        ObjectType = 'Bar'
+        Name       = $Name
+        Value      = $Value
+    }
 }
