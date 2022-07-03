9bea138975baa442d2b672d4eec9af48bdd58f68
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-03 09:28:25 +0200

    Update example

--- Examples/QrCode.ps1
+++ Examples/QrCode.ps1
@@ -1,4 +1,6 @@
 ﻿Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
 
+#Add-Type -Path "C:\Support\GitHub\ImagePlayground\Lib\Default\System.Numerics.Vectors.dll" -PassThru
+
 New-ImageQRCode -Content 'https://evotec.xyz' -FilePath "$PSScriptRoot\Samples\QRCode.png"
-New-ImageQRCode -Content 'https://evotec.xyz' -FilePath "$PSScriptRoot\Samples\QRCodeTransparent.png" -Transparent
+New-ImageQRCode -Content 'https://evotec.xyz' -FilePath "$PSScriptRoot\Samples\QRCodeTransparent.png"
