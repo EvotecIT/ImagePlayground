752f85f82f800e77c4c8118f3f457607ce4fd16e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-24 10:45:47 +0200

    Read barcodes

--- Public/Get-ImageBarCode.ps1
+++ Public/Get-ImageBarCode.ps1
@@ -1,6 +1,12 @@
 ﻿function Get-ImageBarCode {
     [cmdletBinding()]
     param(
-
+        [string] $FilePath
     )
+    if ($FilePath -and (Test-Path -LiteralPath $FilePath)) {
+        $BarCode = [ImagePlayground.BarCode]::Read($FilePath)
+        $BarCode
+    } else {
+        Write-Warning -Message "Get-ImageBarCode - File $FilePath not found. Please check the path."
+    }
 }
