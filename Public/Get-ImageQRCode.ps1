7f37a708fe626a39f88668a51ef53cb05e16513e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-24 10:45:56 +0200

    Read QR Codes

--- Public/Get-ImageQRCode.ps1
+++ Public/Get-ImageQRCode.ps1
@@ -1,6 +1,12 @@
 ﻿function Get-ImageQRCode {
     [cmdletBinding()]
     param(
-
+        [string] $FilePath
     )
+    if ($FilePath -and (Test-Path -LiteralPath $FilePath)) {
+        $QRCode = [ImagePlayground.QRCode]::Read($FilePath)
+        $QRCode
+    } else {
+        Write-Warning -Message "Get-ImageQRCode - File $FilePath not found. Please check the path."
+    }
 }
