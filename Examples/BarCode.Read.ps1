2a1a3a398320845030c95cea07591f687bfdb932
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-24 11:00:58 +0200

    Update examples

--- Examples/BarCode.Read.ps1
+++ Examples/BarCode.Read.ps1
@@ -1,2 +1,4 @@
 ﻿Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
 
+Get-ImageBarCode -FilePath $PSScriptRoot\Samples\BarcodeEAN13.png
+Get-ImageBarCode -FilePath $PSScriptRoot\Samples\BarcodeEAN7.png
--- Examples/ChartsBar.ps1
+++ Examples/ChartsBar.ps1
@@ -1,12 +1,13 @@
 ﻿Import-Module .\ImagePlayground.psd1 -Force
 
+# doesn't work yet
 New-ImageChart {
     New-ImageChartBar -Value 5, 10 -Label "C#"
     New-ImageChartBar -Value 12, 18 -Label "C++"
     New-ImageChartBar -Value 10, 13 -Label "PowerShell"
 } -Show -FilePath $PSScriptRoot\Samples\ChartsBar.png
 
-
+# works fine
 New-ImageChart {
     New-ImageChartBar -Value 5 -Label "C#"
     New-ImageChartBar -Value 12 -Label "C++"
