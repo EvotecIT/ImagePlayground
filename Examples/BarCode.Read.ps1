2de3bb279599ce73f414f1525d46e1149402f184
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-28 17:18:44 +0200

    Updated examples

--- Examples/BarCode.Read.ps1
+++ Examples/BarCode.Read.ps1
@@ -1,4 +1,4 @@
-﻿Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
+﻿Import-Module .\ImagePlayground.psd1 -Force
 
 Get-ImageBarCode -FilePath $PSScriptRoot\Samples\BarcodeEAN13.png
 Get-ImageBarCode -FilePath $PSScriptRoot\Samples\BarcodeEAN7.png
