9d6657e1576941c287ef432ce882e8769cd8cd29
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-28 16:41:04 +0200

    Update module starter pack

--- ImagePlayground.psm1
+++ ImagePlayground.psm1
@@ -1,26 +1,51 @@
-#Get public and private function definition files.
+# Get public and private function definition files.
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue -Recurse )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue -Recurse )
 $Classes = @( Get-ChildItem -Path $PSScriptRoot\Classes\*.ps1 -ErrorAction SilentlyContinue -Recurse )
 $Enums = @( Get-ChildItem -Path $PSScriptRoot\Enums\*.ps1 -ErrorAction SilentlyContinue -Recurse )
-
+# Get all assemblies
 $AssemblyFolders = Get-ChildItem -Path $PSScriptRoot\Lib -Directory -ErrorAction SilentlyContinue
 $Assembly = @(
+    if ($AssemblyFolders.BaseName -contains 'Standard') {
+        @( Get-ChildItem -Path $PSScriptRoot\Lib\Standard\*.dll -ErrorAction SilentlyContinue -Recurse)
+    }
     if ($PSEdition -eq 'Core') {
         @( Get-ChildItem -Path $PSScriptRoot\Lib\Core\*.dll -ErrorAction SilentlyContinue -Recurse )
     } else {
         @( Get-ChildItem -Path $PSScriptRoot\Lib\Default\*.dll -ErrorAction SilentlyContinue -Recurse )
     }
-    if ($AssemblyFolders.BaseName -contains 'Standard') {
-        @( Get-ChildItem -Path $PSScriptRoot\Lib\Standard\*.dll -ErrorAction SilentlyContinue -Recurse)
-    }
 )
+
+# This is special way of importing DLL if multiple frameworks are in use
 $FoundErrors = @(
+    # We load the DLL that does OnImportRemove if we have special module that requires special treatment for binary modules
+
+    # Get library name, from the PSM1 file name
+    $LibraryName = 'ImagePlayground.PowerShell' # $myInvocation.MyCommand.Name.Replace(".psm1", "")
+    $Library = "$LibraryName.dll"
+    $Class = "$LibraryName.Initialize"
+
+    try {
+        $ImportModule = Get-Command -Name Import-Module -Module Microsoft.PowerShell.Core
+        $Framework = if ($PSVersionTable.PSVersion.Major -eq 5) {
+            'Default'
+        } else {
+            'Core'
+        }
+        if (-not ($Class -as [type])) {
+            & $ImportModule ([IO.Path]::Combine($PSScriptRoot, 'Lib', $Framework, $Library)) -ErrorAction Stop
+        } else {
+            $Type = "$Class" -as [Type]
+            & $importModule -Force -Assembly ($Type.Assembly)
+        }
+    } catch {
+        Write-Warning -Message "Importing module $Library failed. Fix errors before continuing. Error: $($_.Exception.Message)"
+        $true
+    }
+
     Foreach ($Import in @($Assembly)) {
         try {
-            Write-Verbose -Message $Import.FullName
             Add-Type -Path $Import.Fullname -ErrorAction Stop
-            #  }
         } catch [System.Reflection.ReflectionTypeLoadException] {
             Write-Warning "Processing $($Import.Name) Exception: $($_.Exception.Message)"
             $LoaderExceptions = $($_.Exception.LoaderExceptions) | Sort-Object -Unique
@@ -39,12 +64,13 @@ $FoundErrors = @(
             #Write-Error -Message "StackTrace: $($_.Exception.StackTrace)"
         }
     }
+
     #Dot source the files
-    Foreach ($Import in @($Private + $Public + $Classes + $Enums)) {
+    Foreach ($Import in @($Private + $Classes + $Enums + $Public)) {
         Try {
             . $Import.Fullname
         } Catch {
-            Write-Error -Message "Failed to import functions from $($import.Fullname): $_"
+            Write-Warning -Message "Failed to import functions from $($import.Fullname).Error: $($_.Exception.Message)"
             $true
         }
     }
@@ -56,4 +82,4 @@ if ($FoundErrors.Count -gt 0) {
     break
 }
 
-Export-ModuleMember -Function '*' -Alias '*'
+Export-ModuleMember -Function '*' -Alias '*' -Cmdlet '*'
