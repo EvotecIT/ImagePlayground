873edbf368ea1f5c9219c486324a3f67c51b97a6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-27 17:53:14 +0100

    Image tests

---
+++ Sources/ImagePlayground.Tests/ImageManipulation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ImagePlayground.Tests {
+    public partial class ImagePlayground {
+        [Fact]
+        public void Test_ImageManipulation() {
+            string filePath = System.IO.Path.Combine(_directoryWithImages, "QRCodeUrlBefore.jpg");
+            File.Delete(filePath);
+            Assert.True(File.Exists(filePath) == false);
+        }
+    }
+}
