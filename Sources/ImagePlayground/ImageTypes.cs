e1814d5b21b45a6005e04bdf749aa8cb59d1c0c8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-27 17:50:24 +0100

    Add image types

---
+++ Sources/ImagePlayground/ImageTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImagePlayground {
+    public enum ImageTypes {
+        Bmp,
+        Gif,
+        Jpeg,
+        Pbm,
+        Png,
+        Tga,
+        Tiff,
+        WebP
+    }
+}
