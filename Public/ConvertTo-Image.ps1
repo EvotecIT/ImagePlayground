18c13fd2cf632c6f376bf9c5af8c0cb357928ad2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-28 18:31:37 +0100

    Add small help to function

--- Public/ConvertTo-Image.ps1
+++ Public/ConvertTo-Image.ps1
@@ -1,4 +1,23 @@
 ﻿function ConvertTo-Image {
+    <#
+    .SYNOPSIS
+    Converts the image to the specified format.
+
+    .DESCRIPTION
+    Converts the image to the specified format. The output path must include the file extension.
+
+    .PARAMETER FilePath
+    File path to the image you want to convert.
+
+    .PARAMETER OutputPath
+    File path to the output image that will be created.
+
+    .EXAMPLE
+    ConvertTo-Image -FilePath $PSScriptRoot\Samples\LogoEvotec.png -OutputPath $PSScriptRoot\Output\LogoEvotec.jpg
+
+    .NOTES
+    General notes
+    #>
     [cmdletBinding()]
     param(
         [parameter(Mandatory)][string] $FilePath,
