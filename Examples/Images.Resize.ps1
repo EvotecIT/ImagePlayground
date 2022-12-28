7b577842626d6a0d71995f34bd28b03c8335f87b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-28 09:49:48 +0100

    Add example on resize

--- Examples/Images.Resize.ps1
+++ Examples/Images.Resize.ps1
@@ -1,3 +1,5 @@
 ﻿Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
 
+Resize-Image -FilePath $PSScriptRoot\Samples\LogoEvotec.png -OutputPath $PSScriptRoot\Samples\LogoEvotecResize.png -Width 100 -Height 100
 
+Resize-Image -FilePath $PSScriptRoot\Samples\LogoEvotec.png -OutputPath $PSScriptRoot\Samples\LogoEvotecResizePercent.png -Percentage 200
