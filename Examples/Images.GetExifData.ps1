4de8ab5f5b8a19ee921588352274e685de8b7b99
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-28 09:34:36 +0100

    Example for EXIF Data

---
+++ Examples/Images.GetExifData.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
+
+$Image = Get-Image -FilePath "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg"
+$Image.Width
+$Image.Height
+$Image.Metadata
+$Image.Metadata.ExifProfile | Format-List
+$Image.Metadata.ExifProfile.Values | Format-Table
+$Image.Metadata.IccProfile.Header | Format-Table
+$Image.Metadata.IccProfile.Entries | Format-Table
