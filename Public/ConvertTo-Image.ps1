f6cf4c9040b7ea850c54edb8e6df47e1168bec17
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-28 09:49:17 +0100

    Add another function

---
+++ Public/ConvertTo-Image.ps1
@@ -0,0 +1,12 @@
+﻿function ConvertTo-Image {
+    [cmdletBinding()]
+    param(
+        [parameter(Mandatory)][string] $FilePath,
+        [parameter(Mandatory)][string] $OutputPath
+    )
+    if ($FilePath -and (Test-Path -LiteralPath $FilePath)) {
+        [ImagePlayground.ImageHelper]::ConvertTo($FilePath, $OutputPath)
+    } else {
+        Write-Warning -Message "Resize-Image - File $FilePath not found. Please check the path."
+    }
+}
