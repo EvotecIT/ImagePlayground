ddadd3a3ba3b5c7e4ac9e99471406b800e7e974b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-28 18:31:49 +0100

    Add small help to function

--- Public/Get-Image.ps1
+++ Public/Get-Image.ps1
@@ -1,4 +1,23 @@
 ﻿function Get-Image {
+    <#
+    .SYNOPSIS
+    Gets the image from the file path for further processing and image manipulation.
+
+    .DESCRIPTION
+    Gets the image from the file path for further processing and image manipulation.
+
+    .PARAMETER FilePath
+    File path to the image you want to read and manipulate.
+
+    .EXAMPLE
+    $Image = Get-Image -FilePath $PSScriptRoot\Samples\LogoEvotec.png
+    $Image.BlackWhite()
+    $Image.BackgroundColor("Red")
+    Save-Image -Image $Image -Open -FilePath $PSScriptRoot\Output\LogoEvotecChanged.png
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param(
         [string] $FilePath
