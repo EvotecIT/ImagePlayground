ec4e87ba5e848367e43489480dab9d96c2e0c50c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-30 19:12:20 +0100

    Add function to crate bar codes

--- Public/New-ImageBarCode.ps1
+++ Public/New-ImageBarCode.ps1
@@ -1,6 +1,9 @@
 ﻿function New-ImageBarCode {
     [cmdletBinding()]
     param(
-
+        [parameter(Mandatory)][ImagePlayground.BarCode+BarcodeTypes] $Type,
+        [parameter(Mandatory)][string] $Value,
+        [parameter(Mandatory)][string] $FilePath
     )
+    [ImagePlayground.BarCode]::Generate($Type, $Value, $filePath)
 }
