abe697620bc79ccab70779f1d68ff22eece5829a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-30 19:11:57 +0100

    Add example to create bar codes

---
+++ Examples/Barcode.Create.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module .\ImagePlayground.psd1 -Force
+
+New-ImageBarCode -FilePath $PSScriptRoot\Samples\BarcodeEAN13.png -Type EAN -Value "5901234123457"
+New-ImageBarCode -FilePath $PSScriptRoot\Samples\BarcodeEAN7.png -Type EAN -Value "5901234"
