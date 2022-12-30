3bf6a070e5dc43c318271dc07d71097462b230b3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-30 19:12:36 +0100

    Add example

---
+++ Examples/Images.Combine.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module .\ImagePlayground.psd1 -Force
+
+$mergeImageSplat = @{
+    FilePath        = "$PSScriptRoot\Samples\BarcodeEAN13.png"
+    FilePathToMerge = "$PSScriptRoot\Samples\BarcodeEAN7.png"
+    FilePathOutput  = "$PSScriptRoot\Output\BarcodeEAN13-7.png"
+    ResizeToFit     = $true
+    Placement       = 'Bottom'
+}
+
+Merge-Image @mergeImageSplat
+
+
+$mergeImageSplat = @{
+    FilePath        = "$PSScriptRoot\Samples\ChartsBar.png"
+    FilePathToMerge = "$PSScriptRoot\Samples\LogoEvotec.png"
+    FilePathOutput  = "$PSScriptRoot\Output\MergedImage.png"
+    ResizeToFit     = $true
+    Placement       = 'Left'
+}
+
+Merge-Image @mergeImageSplat
