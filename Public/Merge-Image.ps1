257d09ab0410f43bf036b54f7f7adb23d614b32e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-30 19:12:06 +0100

    Add function to merge images

---
+++ Public/Merge-Image.ps1
@@ -0,0 +1,19 @@
+﻿function Merge-Image {
+    [CmdletBinding()]
+    param(
+        [string] $FilePath,
+        [string] $FilePathToMerge,
+        [string] $FilePathOutput,
+        [switch] $ResizeToFit,
+        [ImagePlayground.ImagePlacement] $Placement = [ImagePlayground.ImagePlacement]::Bottom
+    )
+    if (-not (Test-Path -LiteralPath $FilePath)) {
+        Write-Warning -Message "Merge-Image - File $FilePath not found. Please check the path."
+        return
+    }
+    if (-not (Test-Path -LiteralPath $FilePathToMerge)) {
+        Write-Warning -Message "Merge-Image - File $FilePathToMerge not found. Please check the path."
+        return
+    }
+    [ImagePlayground.ImageHelper]::Combine($FilePath, $FilePathToMerge, $FilePathOutput, $ResizeToFit.IsPresent, $Placement)
+}
