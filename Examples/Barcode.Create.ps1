bab7905ebdb91c00b9af482a8c450dcdd69f4f83
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-31 17:34:49 +0100

    Update example

--- Examples/Barcode.Create.ps1
+++ Examples/Barcode.Create.ps1
@@ -1,4 +1,7 @@
 ﻿Import-Module .\ImagePlayground.psd1 -Force
 
-New-ImageBarCode -FilePath $PSScriptRoot\Samples\BarcodeEAN13.png -Type EAN -Value "5901234123457"
-New-ImageBarCode -FilePath $PSScriptRoot\Samples\BarcodeEAN7.png -Type EAN -Value "5901234"
+New-ImageBarCode -FilePath C:\Temp\BarcodeEAN13.png -Type EAN -Value "5901234123457"
+New-ImageBarCode -FilePath C:\Temp\BarcodeEAN7.png -Type EAN -Value "5901234"
+
+Get-ImageBarCode -FilePath C:\Temp\BarcodeEAN13.png
+Get-ImageBarCode -FilePath C:\Temp\BarcodeEAN7.png
