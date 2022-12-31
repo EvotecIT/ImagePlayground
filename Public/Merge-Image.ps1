8180499ff15d1d74ec0ee92ae47ce5e468529db8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-31 18:03:21 +0100

    add small doc

--- Public/Merge-Image.ps1
+++ Public/Merge-Image.ps1
@@ -1,4 +1,32 @@
 ﻿function Merge-Image {
+    <#
+    .SYNOPSIS
+    Merges two images into a single image
+
+    .DESCRIPTION
+    Merges two images into a single image
+
+    .PARAMETER FilePath
+    File path to image to be processed for merging of images. This image will be the base image.
+
+    .PARAMETER FilePathToMerge
+    File path to image to be merged into the base image.
+
+    .PARAMETER FilePathOutput
+    File path to output image.
+
+    .PARAMETER ResizeToFit
+    Resize the image to fit the base image. If not specified, the image will be added as is.
+
+    .PARAMETER Placement
+    Placement of the image to be merged. Default is bottom. Possible values: Top, Bottom, Left, Right
+
+    .EXAMPLE
+    Merge-Image -FilePath "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg" -FilePathToMerge "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg" -FilePathOutput "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg" -ResizeToFit -Placement Bottom
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param(
         [string] $FilePath,
