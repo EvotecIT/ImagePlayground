c96366c4c604d6cdf4443853df9f96ae4ece77df
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-31 18:03:37 +0100

    add small doc

--- Public/Save-Image.ps1
+++ Public/Save-Image.ps1
@@ -1,4 +1,29 @@
 ﻿function Save-Image {
+    <#
+    .SYNOPSIS
+    Saves image that was open for processing
+
+    .DESCRIPTION
+    Saves image that was open for processing
+
+    .PARAMETER Image
+    Image object to be saved
+
+    .PARAMETER FilePath
+    File path to where the image should be saved. If not specified, the original image will be overwritten.
+
+    .PARAMETER Open
+    Opens the image in default image viewer after saving
+
+    .EXAMPLE
+    $Image = Get-Image -FilePath $PSScriptRoot\Samples\LogoEvotec.png
+    $Image.BlackWhite()
+    $Image.BackgroundColor("Red")
+    Save-Image -Image $Image -Open -FilePath $PSScriptRoot\Output\LogoEvotecChanged.png
+
+    .NOTES
+    General notes
+    #>
     [cmdletBinding()]
     param(
         [parameter(Mandatory)][ImagePlayground.Image] $Image,
