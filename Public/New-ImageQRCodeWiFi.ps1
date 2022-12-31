8561f43a66b4392b74e69a6819dc4cafb27cc6ec
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-31 18:03:29 +0100

    add small doc

--- Public/New-ImageQRCodeWiFi.ps1
+++ Public/New-ImageQRCodeWiFi.ps1
@@ -1,4 +1,29 @@
 ﻿function New-ImageQRCodeWiFi {
+    <#
+    .SYNOPSIS
+    Creates QR code for WiFi connection
+
+    .DESCRIPTION
+    Creates QR code for WiFi connection
+
+    .PARAMETER SSID
+    SSID of the WiFi network
+
+    .PARAMETER Password
+    Password of the WiFi network
+
+    .PARAMETER FilePath
+    File path to where the image with QR code should be saved.
+
+    .PARAMETER Show
+    Opens the image in default image viewer after saving
+
+    .EXAMPLE
+    New-ImageQRCodeWiFi -SSID 'Evotec' -Password 'Evotec' -FilePath "$PSScriptRoot\Samples\QRCodeWiFi.png"
+
+    .NOTES
+    General notes
+    #>
     [alias('New-QRCodeWiFi')]
     [cmdletBinding()]
     param(
