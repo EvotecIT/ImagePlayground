09d99a5f21cf286f8da934ebbd52c478abf0a662
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-31 18:03:34 +0100

    add small doc

--- Public/Remove-ImageExifTag.ps1
+++ Public/Remove-ImageExifTag.ps1
@@ -1,4 +1,32 @@
 ﻿function Remove-ImageExif {
+    <#
+    .SYNOPSIS
+    Removes EXIF data from image
+
+    .DESCRIPTION
+    Removes EXIF data from image.
+
+    .PARAMETER FilePath
+    File path to image to be processed for Exif Tag removal
+
+    .PARAMETER FilePathOutput
+    File path to where the image with removed Exif Tag should be saved. If not specified, the original image will be overwritten.
+
+    .PARAMETER ExifTag
+    Exif Tag to be removed from image
+
+    .PARAMETER All
+    Removes all Exif Tags from image
+
+    .EXAMPLE
+    Remove-ImageExif -FilePath "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg" -ExifTag [SixLabors.ImageSharp.Metadata.Profiles.Exif.ExifTag]::ExposureTime
+
+    .EXAMPLE
+    Remove-ImageExif -FilePath "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg" -All
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding(DefaultParameterSetName = 'RemoveExifTag')]
     param(
         [Parameter(Mandatory, ParameterSetName = 'All')]
