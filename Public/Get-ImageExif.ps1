c93f20794e5a87b393ce8a2dddba9116e815813d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-31 17:21:44 +0100

    Add get-exiftag

---
+++ Public/Get-ImageExif.ps1
@@ -0,0 +1,24 @@
+﻿function Get-ImageExif {
+    <#
+    .SYNOPSIS
+    Gets EXIF data from image
+
+    .DESCRIPTION
+    Gets EXIF data from image.
+
+    .PARAMETER FilePath
+    File path to image to be processed for Exif Tag reading
+
+    .EXAMPLE
+    Get-ImageExif -FilePath "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg"
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string] $FilePath
+    )
+    $Image = Get-Image -FilePath $FilePath
+    $Image.Metadata.ExifProfile.Values
+}
