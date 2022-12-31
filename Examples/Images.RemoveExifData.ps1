83285ae3f13ab9d5a5df4b9cff8e94efc19b9bd9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-31 17:35:00 +0100

    Add examples with Exif

---
+++ Examples/Images.RemoveExifData.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
+
+Get-ImageExif -FilePath "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg" | Format-Table
+
+$removeImageExifSplat = @{
+    FilePath       = "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg"
+    ExifTag        = [SixLabors.ImageSharp.Metadata.Profiles.Exif.ExifTag]::GPSLatitude, [SixLabors.ImageSharp.Metadata.Profiles.Exif.ExifTag]::GPSLongitude
+    FilePathOutput = "$PSScriptRoot\Output\IMG_46441.jpeg"
+}
+Remove-ImageExif @removeImageExifSplat
+
+$removeImageExifSplat = @{
+    FilePath       = "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg"
+    All            = $true
+    FilePathOutput = "$PSScriptRoot\Output\IMG_46442.jpeg"
+}
+Remove-ImageExif @removeImageExifSplat
+
+Get-ImageExif -FilePath $PSScriptRoot\Output\IMG_46441.jpeg | Format-Table
+Get-ImageExif -FilePath $PSScriptRoot\Output\IMG_46442.jpeg | Format-Table
---
+++ Examples/Images.SetExifData.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
+
+Get-ImageExif -FilePath "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg" -Translate | Format-List Datetime*, GPS*
+
+$setImageExifSplat = @{
+    FilePath       = "C:\Users\przemyslaw.klys\Downloads\IMG_4644.jpeg"
+    ExifTag        = ([SixLabors.ImageSharp.Metadata.Profiles.Exif.ExifTag]::DateTimeOriginal)
+    Value          = ([DateTime]::Now).ToString("yyyy:MM:dd HH:mm:ss")
+    FilePathOutput = "$PSScriptRoot\Output\IMG_4644.jpeg"
+}
+
+Set-ImageExif @setImageExifSplat
+
+Get-ImageExif -FilePath $PSScriptRoot\Output\IMG_4644.jpeg -Translate | Format-List Datetime*, GPS*
