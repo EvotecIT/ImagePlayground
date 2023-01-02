e847f866b18c8d4a9b801c090228bb9f6db34380
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-02 10:17:22 +0100

    Add example for radial chart

---
+++ Examples/Charts.Radial.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
+
+New-ImageChart {
+    New-ImageChartRadial -Name "C#" -Value 5
+    New-ImageChartRadial -Name "AutoIt v3" -Value 50
+    New-ImageChartRadial -Name "PowerShell" -Value 10
+    New-ImageChartRadial -Name "C++" -Value 18
+    New-ImageChartRadial -Name "F#" -Value 100
+} -Show -FilePath $PSScriptRoot\Samples\ChartsRadial.png -Width 500 -Height 500
