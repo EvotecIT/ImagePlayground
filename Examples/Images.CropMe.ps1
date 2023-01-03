7bd9726262d8b806dfca452296d5aff846370a14
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-03 11:22:27 +0100

    Crop example

---
+++ Examples/Images.CropMe.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
+
+$Image = Get-Image -FilePath $PSScriptRoot\Samples\LogoEvotec.png
+$Rectangle = [SixLabors.ImageSharp.Rectangle]::new(30, 30, 400, 100)
+$Image.Crop($Rectangle)
+Save-Image -Image $Image -Open -FilePath $PSScriptRoot\Output\LogoEvotecChanged.png
