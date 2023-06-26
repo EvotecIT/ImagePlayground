b4620ea5681b75fbbf5ea0539e810975526083ef
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-06-26 19:25:36 +0200

    Additional example

---
+++ Examples/Images.Brightness.ps1
@@ -0,0 +1,8 @@
+﻿
+$File = "C:\Support\GitHub\ImagePlayground\Examples\Samples\PrzemyslawKlysAndKulkozaurr.jpg"
+
+$Image = Get-Image -FilePath $File
+$Image.Brightness(0.5)
+$Image.Save($File.Replace('.jpg', '_Brightness1.jpg'))
+$Image.Brightness(0.1)
+$Image.Save($File.Replace('.jpg', '_Brightness2.jpg'))
