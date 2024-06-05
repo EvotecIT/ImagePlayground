4d820ac435f5dc2c90fcc920eb13fda71cf4c3db
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-05 19:20:01 +0200

    Update watermark example

--- Examples/Images.Watermark1.ps1
+++ Examples/Images.Watermark1.ps1
@@ -3,6 +3,12 @@
 # Add watermark
 $Image = Get-Image -FilePath "$PSScriptRoot\Samples\PrzemyslawKlysAndKulkozaurr.jpg"
 # void WatermarkImage(string filePath, ImagePlayground.Image+WatermarkPlacement placement, float opacity = 1, float padding = 18, int rotate = 0, SixLabors.ImageSharp.Processing.FlipMode flipMode = SixLabors.ImageSharp.Processing.FlipMode.None, int watermarkPercentage = 20)
+
+# place in the middle
 $Image.WatermarkImage("$PSScriptRoot\Samples\LogoEvotec.png", [ImagePlayground.Image+WatermarkPlacement]::Middle, 0.5, 0.5)
+
+# place with x,y
+$Image.WatermarkImage("$PSScriptRoot\Samples\LogoEvotec.png", 50, 100, 0.5, 0.5)
+
 # Add watermark with rotation 90 degrees
 Save-Image -Image $Image -Open -FilePath $PSScriptRoot\Output\QRCodeWithImage.jpg
