19c13e6562325743e9274f6ccecc194780e944a1
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-16 18:36:40 +0200

    Fix font fallback for AddText

--- Sources/ImagePlayground/Image.Text.cs
+++ Sources/ImagePlayground/Image.Text.cs
@@ -9,7 +9,9 @@ namespace ImagePlayground {
     public partial class Image : IDisposable {
         public FontRectangle GetTextSize(string text, float fontSize, string fontFamilyName) {
             if (!SixLabors.Fonts.SystemFonts.TryGet(fontFamilyName, out var fontFamily)) {
-                fontFamily = SixLabors.Fonts.SystemFonts.Collection.Families.First();
+                if (!SixLabors.Fonts.SystemFonts.TryGet("DejaVu Sans", out fontFamily)) {
+                    fontFamily = SixLabors.Fonts.SystemFonts.Collection.Families.First();
+                }
             }
             var font = fontFamily.CreateFont(fontSize, FontStyle.Regular);
             var options = new TextOptions(font) { Dpi = 72, KerningMode = KerningMode.Auto };
@@ -19,7 +21,9 @@ namespace ImagePlayground {
 
         public void AddText(float x, float y, string text, SixLabors.ImageSharp.Color color, float fontSize = 16f, string fontFamilyName = "Arial") {
             if (!SixLabors.Fonts.SystemFonts.TryGet(fontFamilyName, out var fontFamily)) {
-                fontFamily = SixLabors.Fonts.SystemFonts.Collection.Families.First();
+                if (!SixLabors.Fonts.SystemFonts.TryGet("DejaVu Sans", out fontFamily)) {
+                    fontFamily = SixLabors.Fonts.SystemFonts.Collection.Families.First();
+                }
             }
             var font = fontFamily.CreateFont(fontSize, FontStyle.Regular);
             var pointF = new PointF(x, y);
