e9daa5244fa30649b1926279914e42e021d40da8
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-17 19:55:36 +0200

    Reset stream position after saving avatar

---
+++ Sources/ImagePlayground.Tests/AvatarStream.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using Xunit;
+
+namespace ImagePlayground.Tests {
+    public partial class ImagePlayground {
+        [Fact]
+        public void Test_SaveAsAvatar_StreamReset() {
+            string filePath = Path.Combine(_directoryWithImages, "LogoEvotec.png");
+            using var img = Image.Load(filePath);
+            using var ms = new MemoryStream();
+            img.SaveAsAvatar(ms, 64, 64, 10f);
+            Assert.Equal(0, ms.Position);
+            Assert.True(ms.Length > 0);
+        }
+    }
+}
