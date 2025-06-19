2f6dba3c3fe05ed436b03d8ba9309f56caebb795
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-19 17:47:58 +0200

    Add annotation examples

---
+++ Examples/Charts.Annotation.ps1
@@ -0,0 +1,8 @@
+Import-Module $PSScriptRoot\..\ImagePlayground.psd1 -Force
+
+New-ImageChart -ChartsDefinition {
+    New-ImageChartLine -Name 'Sample' -Value 1,3,2,5
+} -AnnotationsDefinition {
+    New-ImageChartAnnotation -X 3 -Y 5 -Text 'Peak' -Arrow
+} -Show -FilePath $PSScriptRoot\Samples\ChartsAnnotated.png -Width 500 -Height 300
+
---
+++ Sources/ImagePlayground.PowerShell/CmdletNewImageChartAnnotation.cs
@@ -0,0 +1,32 @@
+using System.Management.Automation;
+
+namespace ImagePlayground.PowerShell;
+
+/// <summary>Creates chart annotation data item.</summary>
+/// <example>
+///   <summary>Create annotation</summary>
+///   <code>New-ImageChartAnnotation -X 1 -Y 2 -Text 'Hello' -Arrow</code>
+/// </example>
+[Cmdlet(VerbsCommon.New, "ImageChartAnnotation")]
+public sealed class NewImageChartAnnotationCmdlet : PSCmdlet {
+    /// <summary>X coordinate for annotation.</summary>
+    [Parameter(Mandatory = true, Position = 0)]
+    public double X { get; set; }
+
+    /// <summary>Y coordinate for annotation.</summary>
+    [Parameter(Mandatory = true, Position = 1)]
+    public double Y { get; set; }
+
+    /// <summary>Annotation text.</summary>
+    [Parameter(Mandatory = true, Position = 2)]
+    public string Text { get; set; } = string.Empty;
+
+    /// <summary>Display arrow.</summary>
+    [Parameter]
+    public SwitchParameter Arrow { get; set; }
+
+    /// <inheritdoc />
+    protected override void ProcessRecord() {
+        WriteObject(new ImagePlayground.Charts.ChartAnnotation(X, Y, Text, Arrow.IsPresent));
+    }
+}
