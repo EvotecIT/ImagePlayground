bc2c6880d639504de50d3b378ec9849312236382
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-20 22:42:51 +0200

    refactor(powershell): 🔨 tidy module import hooks

--- Sources/ImagePlayground.PowerShell/OnImportAndRemove.cs
+++ Sources/ImagePlayground.PowerShell/OnImportAndRemove.cs
@@ -19,7 +19,7 @@ public class OnModuleImportAndRemove : IModuleAssemblyInitializer, IModuleAssemb
     /// </summary>
     public void OnImport() {
         if (IsNetFramework()) {
-            AppDomain.CurrentDomain.AssemblyResolve += MyResolveEventHandler;
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
         }
 #if NET5_0_OR_GREATER
         else {
@@ -34,7 +34,7 @@ public class OnModuleImportAndRemove : IModuleAssemblyInitializer, IModuleAssemb
     /// <param name="module"></param>
     public void OnRemove(PSModuleInfo module) {
         if (IsNetFramework()) {
-            AppDomain.CurrentDomain.AssemblyResolve -= MyResolveEventHandler;
+            AppDomain.CurrentDomain.AssemblyResolve -= ResolveAssembly;
         }
 #if NET5_0_OR_GREATER
         else {
@@ -44,12 +44,12 @@ public class OnModuleImportAndRemove : IModuleAssemblyInitializer, IModuleAssemb
     }
 
     /// <summary>
-    /// MyResolveEventHandler is a method that handles the AssemblyResolve event.
+    /// ResolveAssembly is a method that handles the AssemblyResolve event.
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="args"></param>
     /// <returns></returns>
-    private static Assembly? MyResolveEventHandler(object? sender, ResolveEventArgs args) {
+    private static Assembly? ResolveAssembly(object? sender, ResolveEventArgs args) {
         var libDirectory = Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location);
         var directoriesToSearch = new List<string>();
 
@@ -113,20 +113,4 @@ public class OnModuleImportAndRemove : IModuleAssemblyInitializer, IModuleAssemb
         return System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework", StringComparison.OrdinalIgnoreCase);
     }
 
-    // Determine if the current runtime is .NET Core
-    private bool IsNetCore() {
-        return System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET Core", StringComparison.OrdinalIgnoreCase);
-    }
-
-    /// <summary>
-    /// Determine if the current runtime is .NET 5 or higher
-    /// </summary>
-    /// <returns></returns>
-    private bool IsNet5OrHigher() {
-        return System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 5", StringComparison.OrdinalIgnoreCase) ||
-               System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 6", StringComparison.OrdinalIgnoreCase) ||
-               System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 7", StringComparison.OrdinalIgnoreCase) ||
-               System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 8", StringComparison.OrdinalIgnoreCase) ||
-               System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 9", StringComparison.OrdinalIgnoreCase);
-    }
 }
