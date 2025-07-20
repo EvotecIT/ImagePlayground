0b32356f72477c4e0dc0ab52b3cd35c0c1df7773
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-20 22:35:46 +0200

    test: 🧪 remove module import

--- Tests/LoadContext.Tests.ps1
+++ Tests/LoadContext.Tests.ps1
@@ -1,7 +1,4 @@
 Describe 'Assembly Load Context' {
-    BeforeAll {
-        Import-Module "$PSScriptRoot/../ImagePlayground.psd1" -Force
-    }
 
     It 'creates a dedicated context on import' -Skip:($PSVersionTable.PSVersion.Major -lt 7) {
         $context = [System.Runtime.Loader.AssemblyLoadContext]::All | Where-Object Name -eq 'ImagePlayground'
