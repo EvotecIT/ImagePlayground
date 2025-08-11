a7e02e2a5ee6f81d7593ddbb89cd4c6c3efd1b1d
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-11 13:17:29 +0200

    refactor(powershell): ♻️ rename blur and sharpen cmdlets to approved verbs

---
+++ Tests/Set-ImageSharpen.Tests.ps1
@@ -0,0 +1,35 @@
+Describe 'Set-ImageSharpen' {
+
+    BeforeAll {
+
+        Import-Module "$PSScriptRoot/../ImagePlayground.psd1" -Force
+
+        $TestDir = Join-Path $PSScriptRoot 'Artifacts'
+
+        if (-not (Test-Path $TestDir)) { New-Item -Path $TestDir -ItemType Directory | Out-Null }
+    }
+
+    It 'sharpens an image' {
+        $src = Join-Path $PSScriptRoot '../Sources/ImagePlayground.Tests/Images/LogoEvotec.png'
+        $dest = Join-Path $TestDir 'logo-sharp.png'
+        Set-ImageSharpen -FilePath $src -OutputPath $dest -Amount 2
+        $orig = [ImagePlayground.Image]::Load($src)
+        $img = [ImagePlayground.Image]::Load($dest)
+        $img.Width | Should -Be $orig.Width
+        $img.Height | Should -Be $orig.Height
+        $img.Dispose()
+        $orig.Dispose()
+    }
+
+    It 'sharpens an image asynchronously' {
+        $src = Join-Path $PSScriptRoot '../Sources/ImagePlayground.Tests/Images/LogoEvotec.png'
+        $dest = Join-Path $TestDir 'logo-sharp-async.png'
+        Set-ImageSharpen -FilePath $src -OutputPath $dest -Amount 2 -Async
+        $orig = [ImagePlayground.Image]::Load($src)
+        $img = [ImagePlayground.Image]::Load($dest)
+        $img.Width | Should -Be $orig.Width
+        $img.Height | Should -Be $orig.Height
+        $img.Dispose()
+        $orig.Dispose()
+    }
+}
