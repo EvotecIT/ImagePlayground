29de919111482ec3c378d3202582b21958c8124b
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-04-03 08:33:21 +0200

    refactor(build): 🔥 remove obsolete build scripts
    * Deleted `Build-Package.ps1`, `Publish-PackageGitHub.ps1`, `Publish-PackageNuget.ps1`, and `Update-Version.ps1` as they are no longer needed.
    * Streamlines the build process by eliminating unused files.

--- Build/Update-Version.ps1
+++
@@ -1,6 +0,0 @@
-﻿Import-Module PSPublishModule -Force -ErrorAction Stop
-
-$Path = "$PSScriptRoot\.."
-
-Get-ProjectVersion -Path "$Path" -ExcludeFolders @("Artefacts") | Format-Table
-Set-ProjectVersion -Path "$Path" -NewVersion "1.0.0" -WhatIf:$false -Verbose -ExcludeFolders @("Artefacts") | Format-Table
