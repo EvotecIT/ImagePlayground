253cdcf3edff771b45bc14f76fcb952a95a241a3
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-07-04 20:06:01 +0200

    update

--- Build/Build-Module.ps1
+++ Build/Build-Module.ps1
@@ -96,10 +96,10 @@ Build-Module -ModuleName 'ImagePlayground' -RunMode $RunMode {
     $newConfigurationBuildSplat = @{
         Enable                            = $true
         # lets sign module only on my machine for now
-        SignModule                        = if ($Env:COMPUTERNAME -eq 'EVOMONSTER') { $true } else { $false }
+        SignModule                        = $true
         MergeModuleOnBuild                = $true
         MergeFunctionsFromApprovedModules = $true
-        CertificateThumbprint             = '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
+        CertificateThumbprint             = '92e95fb58effa6a4a75e77a33cdd6bfe6dd30f1a'
         #DotSourceLibraries                = $false
         #DotSourceClasses                  = $false
         #SeparateFileLibraries             = $true
