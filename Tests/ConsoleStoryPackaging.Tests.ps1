3a7277924461b1555d9781aa5ceb7327cab253cc
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-08-01 16:57:17 +0200

    fix(console): 🐛 Complete packaged story help contracts

---
+++ Tests/ConsoleStoryPackaging.Tests.ps1
@@ -0,0 +1,32 @@
+Describe 'Image console story packaging contracts' {
+    It 'keeps checked-in external help complete for the story surface' {
+        $helpPath = Join-Path -Path $PSScriptRoot -ChildPath '..\en-US\ImagePlayground-help.xml'
+        [xml] $help = Get-Content -Path $helpPath -Raw
+        $commands = @(
+            'New-ImageConsoleStory'
+            'New-ImageConsoleStoryBlankLine'
+            'New-ImageConsoleStoryCommand'
+            'New-ImageConsoleStoryOutput'
+            'New-ImageConsoleStoryPause'
+            'New-ImageConsoleStoryTable'
+            'New-ImageVisualMotionCue'
+            'New-ImageVisualStory'
+        )
+
+        foreach ($commandName in $commands) {
+            $command = $help.SelectSingleNode("//*[local-name()='command'][*[local-name()='details']/*[local-name()='name' and text()='$commandName']]")
+            $command | Should -Not -BeNullOrEmpty
+        }
+
+        $consoleStory = $help.SelectSingleNode("//*[local-name()='command'][*[local-name()='details']/*[local-name()='name' and text()='New-ImageConsoleStory']]")
+        $parameterNames = @($consoleStory.SelectNodes(".//*[local-name()='parameter']/*[local-name()='name']") | ForEach-Object InnerText)
+        $parameterNames | Should -Contain 'Content'
+        $parameterNames | Should -Contain 'Step'
+    }
+
+    It 'declares the story step type accelerator for packaged imports' {
+        $buildScript = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\Build\Build-Module.ps1') -Raw
+
+        $buildScript | Should -Match ([regex]::Escape("'ImagePlayground.PowerShell.ImageConsoleStoryStep'"))
+    }
+}
